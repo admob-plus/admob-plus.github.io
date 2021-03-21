ad18b81b0226e0b918c5d9dd9540243ca67e1133
ratson <github@ratson.name>
2021-03-21 11:31:01 +0000

    Deploy website - based on d37de48a941c3f7a0f568e68c50b28ea10fb08b8

---
+++ assets/js/cf97e6d2.b7a7ee88.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[79],{148:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"cordova","name":"cordova","count":2,"permalink":"/blog/tags/cordova"}')}}]);
